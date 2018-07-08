c12c0ac2e7cf519106b336c006cc11333b9a3031
Austin Mayden <austin.mayden@gmail.com>
2018-07-08 20:58:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destuctivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
